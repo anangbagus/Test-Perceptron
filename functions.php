c1ee65bd54455fb939f0af02ce510b85c6d1e33b
Alim Ikegami <alimikegami1@gmail.com>
2021-06-30 20:45:54 +0800

    adding functions.php files

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php 
+
+// connect to database
+$conn = mysqli_connect("localhost", "root", "", "db_hotel");
+
+// read data
+function read($query) {
+	global $conn;
+	$result = mysqli_query($conn, $query);
+	$records = [];
+	while ($tuples = mysqli_fetch_assoc($result)) {
+		$records[] = $tuples;
+	}
+	return $records;
+}
+
+function execute_query($query) {
+	global $conn;
+	mysqli_query($conn, $query);
+	if (mysqli_affected_rows($conn) > 0) {
+		return true;
+	}
+	return false;
+}
+
+function execute_multi_query($query) {
+	global $conn;
+	mysqli_multi_query($conn, $query);
+	if (mysqli_affected_rows($conn) > 0) {
+		return true;
+	}
+	return false;
+}
+
+?>
