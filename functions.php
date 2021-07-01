f046046b7e046f70a2bcd6e37a5f15920b291061
WayanPande <yande554@gmail.com>
2021-07-01 14:06:24 +0800

    test

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
 // connect to database
 $conn = mysqli_connect("localhost", "root", "", "db_hotel");
 
 // read data
-function read($query) {
+function read($query)
+{
 	global $conn;
 	$result = mysqli_query($conn, $query);
 	$records = [];
@@ -14,7 +15,8 @@ function read($query) {
 	return $records;
 }
 
-function execute_query($query) {
+function execute_query($query)
+{
 	global $conn;
 	mysqli_query($conn, $query);
 	if (mysqli_affected_rows($conn) > 0) {
@@ -23,7 +25,8 @@ function execute_query($query) {
 	return false;
 }
 
-function execute_multi_query($query) {
+function execute_multi_query($query)
+{
 	global $conn;
 	mysqli_multi_query($conn, $query);
 	if (mysqli_affected_rows($conn) > 0) {
@@ -31,5 +34,3 @@ function execute_multi_query($query) {
 	}
 	return false;
 }
-
-?>
