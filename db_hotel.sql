b0d62cb1afc83a784b31a02d22b153bfd09131e4
Alim Ikegami <alimikegami1@gmail.com>
2021-07-01 14:06:54 +0800

    updating database

--- db_hotel.sql
+++ db_hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2021 at 02:27 PM
+-- Generation Time: Jul 01, 2021 at 08:05 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -41,8 +41,31 @@ CREATE TABLE `gambar_kamar` (
 
 CREATE TABLE `kamar_hotel` (
   `ID_kamar` int(11) NOT NULL,
-  `ID_tipe_kamar` int(11) NOT NULL
+  `ID_tipe_kamar` int(11) NOT NULL,
+  `status_kamar` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kamar_hotel`
+--
+
+INSERT INTO `kamar_hotel` (`ID_kamar`, `ID_tipe_kamar`, `status_kamar`) VALUES
+(1, 1, 0),
+(2, 1, 0),
+(3, 1, 0),
+(4, 1, 0),
+(5, 2, 0),
+(6, 2, 0),
+(7, 2, 0),
+(8, 2, 0),
+(9, 3, 0),
+(10, 3, 0),
+(11, 3, 0),
+(12, 3, 0),
+(13, 4, 0),
+(14, 4, 0),
+(15, 4, 0),
+(16, 4, 0);
 
 -- --------------------------------------------------------
 
@@ -52,11 +75,23 @@ CREATE TABLE `kamar_hotel` (
 
 CREATE TABLE `reservasi_kamar` (
   `ID_reservasi` int(11) NOT NULL,
-  `ID_user` int(11) NOT NULL,
   `ID_kamar` int(11) NOT NULL,
   `tgl_checkin` datetime NOT NULL,
-  `tgl_checkout` datetime NOT NULL
+  `tgl_checkout` datetime NOT NULL,
+  `nama_customer` varchar(35) NOT NULL,
+  `NIK_customer` char(16) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `nomor_hp` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservasi_kamar`
+--
+
+INSERT INTO `reservasi_kamar` (`ID_reservasi`, `ID_kamar`, `tgl_checkin`, `tgl_checkout`, `nama_customer`, `NIK_customer`, `email`, `nomor_hp`) VALUES
+(5, 1, '2021-06-01 23:10:28', '2021-06-02 23:10:28', 'Alim Ikegami', '1908561046', 'alimikegami1@gmail.com', '081239990127'),
+(6, 1, '2021-06-03 23:10:28', '2021-06-04 23:10:28', 'Alim Ikegami', '1908561046', 'alimikegami1@gmail.com', '081239990127'),
+(7, 1, '2021-06-05 23:18:21', '2021-06-06 23:18:21', 'Alim Ikegami', '1908561046', 'alimikegami1@gmail.com', '081239990127');
 
 -- --------------------------------------------------------
 
@@ -71,20 +106,15 @@ CREATE TABLE `tipe_kamar` (
   `kapasitas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `user_account`
+-- Dumping data for table `tipe_kamar`
 --
 
-CREATE TABLE `user_account` (
-  `ID_user` int(11) NOT NULL,
-  `nama` varchar(30) NOT NULL,
-  `alamat` varchar(75) NOT NULL,
-  `NIK` char(16) NOT NULL,
-  `role` enum('user','admin') NOT NULL,
-  `jenis_kelamin` enum('Pria','Wanita') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `tipe_kamar` (`ID_tipe_kamar`, `tipe`, `harga_per_malam`, `kapasitas`) VALUES
+(1, 'standart', 20000, 100),
+(2, 'superior', 30000, 100),
+(3, 'junior suite', 50000, 100),
+(4, 'deluxe', 40000, 100);
 
 --
 -- Indexes for dumped tables
@@ -109,7 +139,6 @@ ALTER TABLE `kamar_hotel`
 --
 ALTER TABLE `reservasi_kamar`
   ADD PRIMARY KEY (`ID_reservasi`),
-  ADD KEY `ID_user` (`ID_user`),
   ADD KEY `ID_kamar` (`ID_kamar`);
 
 --
@@ -117,12 +146,6 @@ ALTER TABLE `reservasi_kamar`
 --
 ALTER TABLE `tipe_kamar`
   ADD PRIMARY KEY (`ID_tipe_kamar`);
-
---
--- Indexes for table `user_account`
---
-ALTER TABLE `user_account`
-  ADD PRIMARY KEY (`ID_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -138,25 +161,19 @@ ALTER TABLE `gambar_kamar`
 -- AUTO_INCREMENT for table `kamar_hotel`
 --
 ALTER TABLE `kamar_hotel`
-  MODIFY `ID_kamar` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `reservasi_kamar`
 --
 ALTER TABLE `reservasi_kamar`
-  MODIFY `ID_reservasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_reservasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tipe_kamar`
 --
 ALTER TABLE `tipe_kamar`
-  MODIFY `ID_tipe_kamar` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user_account`
---
-ALTER TABLE `user_account`
-  MODIFY `ID_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_tipe_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -178,7 +195,6 @@ ALTER TABLE `kamar_hotel`
 -- Constraints for table `reservasi_kamar`
 --
 ALTER TABLE `reservasi_kamar`
-  ADD CONSTRAINT `reservasi_kamar_ibfk_1` FOREIGN KEY (`ID_user`) REFERENCES `user_account` (`ID_user`),
   ADD CONSTRAINT `reservasi_kamar_ibfk_2` FOREIGN KEY (`ID_kamar`) REFERENCES `kamar_hotel` (`ID_kamar`);
 COMMIT;
 
